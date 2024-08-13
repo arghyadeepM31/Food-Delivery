feefdb8ff921e5fd9fa74924539cfca74474631d
arghyadeepM31 <105260623+arghyadeepM31@users.noreply.github.com>
2024-08-14 01:02:03 +0530

    Add files via upload

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb+srv://arghyadeep:23102@cluster0.uousp.mongodb.net/food-del').then(() => console.log("DB Connected"));
+
+   
+}
+
+
+// add your mongoDB connection string above.
+// Do not use '@' symbol in your databse user's password else it will show an error.
